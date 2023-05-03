c856b6b1fc760c695a90c4231551a3fa0dabff25
ManuelF <manuel.floriani1999@gmail.com>
2023-05-03 16:06:13 +0200

    creazione HTML + JS [Mail]

---
+++ mail/main.js
@@ -0,0 +1,12 @@
+// Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+
+// Dichiara lista invitati
+const invitati = ["ciao@mail.com", "manuel@gmail.com", "boolean@mail.com", "mail@boolean.com", "com@mail.boolean", "studentepazzo04@gmail.com", "hackerino@mail.tor", "pizza@pasta.it"]
+
+// chiedi mail a user
+const mail_user = prompt("Scrivi qui la tua mail");
+// confronta mail user a lista invitati
+for()
+    // SE mail_user = 1 elemento lista invitati ----> "ti aspetto al partyyyy!!!!"
+
+    // ALTRIMENTI ----> "mi dispiace non sei in lista :("
